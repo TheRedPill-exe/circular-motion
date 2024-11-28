1b5adfb5f4cabe4182beb1a6cfac124d0bab721f
Pablo Marín <pablo.marin1@utp.edu.co>
2024-11-28 00:27:09 -0500

    responsive

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -7,25 +7,34 @@ const Videos = () => {
       <h1>Simulación de Movimiento Circular</h1>
 
       <h3 className="video-title">Movimiento Acelerado</h3>
-      <ReactPlayer 
-        url="https://youtu.be/Um71KajKkwg"  // Video de YouTube
-        width="600px" 
-        controls 
-      />
+      <div className="video-wrapper">
+        <ReactPlayer 
+          url="https://youtu.be/Um71KajKkwg"  // Video de YouTube
+          width="100%" 
+          height="auto" 
+          controls 
+        />
+      </div>
 
       <h3 className="video-title">Movimiento sin Aceleración</h3>
-      <ReactPlayer 
-        url="https://youtu.be/gtkjtvEM9LQ"  // Coloca el link de YouTube para este video
-        width="600px" 
-        controls 
-      />
+      <div className="video-wrapper">
+        <ReactPlayer 
+          url="https://youtu.be/gtkjtvEM9LQ"  // Coloca el link de YouTube para este video
+          width="100%" 
+          height="auto" 
+          controls 
+        />
+      </div>
 
       <h3 className="video-title">Fuerza Aplicada en un Momento Específico</h3>
-      <ReactPlayer 
-        url="https://youtu.be/888lL5xLkv0"  // Coloca el link de YouTube para este video
-        width="600px" 
-        controls 
-      />
+      <div className="video-wrapper">
+        <ReactPlayer 
+          url="https://youtu.be/888lL5xLkv0"  // Coloca el link de YouTube para este video
+          width="100%" 
+          height="auto" 
+          controls 
+        />
+      </div>
     </div>
   );
 };
