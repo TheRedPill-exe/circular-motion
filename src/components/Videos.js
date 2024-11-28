7f4f946f603b18aea1d2ff6d84b8a11d99dbe822
Pablo Marín <pablo.marin1@utp.edu.co>
2024-11-28 00:32:24 -0500

    asdf

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactPlayer from 'react-player';  // Asegúrate de tener instalada la librería
+import ReactPlayer from 'react-player';
 
 const Videos = () => {
   return (
@@ -10,8 +10,7 @@ const Videos = () => {
       <div className="video-wrapper">
         <ReactPlayer 
           url="https://youtu.be/Um71KajKkwg"  // Video de YouTube
-          width="100%" 
-          height="auto" 
+          className="react-player"
           controls 
         />
       </div>
@@ -19,9 +18,8 @@ const Videos = () => {
       <h3 className="video-title">Movimiento sin Aceleración</h3>
       <div className="video-wrapper">
         <ReactPlayer 
-          url="https://youtu.be/gtkjtvEM9LQ"  // Coloca el link de YouTube para este video
-          width="100%" 
-          height="auto" 
+          url="https://youtu.be/gtkjtvEM9LQ"  // Video de YouTube
+          className="react-player"
           controls 
         />
       </div>
@@ -29,9 +27,8 @@ const Videos = () => {
       <h3 className="video-title">Fuerza Aplicada en un Momento Específico</h3>
       <div className="video-wrapper">
         <ReactPlayer 
-          url="https://youtu.be/888lL5xLkv0"  // Coloca el link de YouTube para este video
-          width="100%" 
-          height="auto" 
+          url="https://youtu.be/888lL5xLkv0"  // Video de YouTube
+          className="react-player"
           controls 
         />
       </div>
